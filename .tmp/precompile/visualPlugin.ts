28a5e6b63c39a4b61bd0e113694bd7b970c7025c
jatingupta <jatin13csu055@ncuindia.edu>
2018-04-11 21:10:30 +0530

    First Commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var myTestVisual10095615CFC04519822C45B0DA9EA775_DEBUG = {
+        name: 'myTestVisual10095615CFC04519822C45B0DA9EA775_DEBUG',
+        displayName: 'myTestVisual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.11.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.myTestVisual10095615CFC04519822C45B0DA9EA775.Visual(options),
+        custom: true
+    };
+}
